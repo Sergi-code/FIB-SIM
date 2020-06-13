7cb4166f35981f60c069f82aaea6326f805be93f
Sergi-code <61187338+Sergi-code@users.noreply.github.com>
2020-06-13 13:05:48 +0200

    refacherito sin comentarios

--- Client.py
+++ Client.py
@@ -17,7 +17,6 @@ class Client:
         self.num = 0  
         hora = (env.now / 3600)
         self.op.write(str(hora) + ' ' + str(ident) + ": Acabo d'arribar\n")
-        ##print("%7.4f %s: Acabo d'arribar" % (env.now, ident))
         self.op.write("Vull comprar " + str(self.numProds) + " productes\n")
 
     def comprant(self):
@@ -93,56 +92,3 @@ class Client:
         if self.st.numCaixesTrad == 0 : self.st.setTempsSuper(1, self.env.now - self.tempsEntraSuper)
         elif self.st.numCaixesAuto == 0: self.st.setTempsSuper(0, self.env.now - self.tempsEntraSuper)
         else: self.st.setTempsSuper(self.auto, self.env.now - self.tempsEntraSuper)
-            
-
-    '''def agafarProd(self):
-        print("hey") #debug
-        numEstanteria = math.floor(random.uniform(0, 5.99))
-        with self.sp.estanteries[numEstanteria].request() as req:
-            print("hey2") #debug
-            yield req
-            print("hey3") #debug
-            self.num += 1
-            print("Client: " + str(self.id) + ", porto:" + str(self.num) + " productes")'''
-    
-    '''def anarCuaAuto(self):
-        print("Client: " + str(self.id) + " estic esperant a la cua d'autoservei")
-        with self.sp.cues[0].request() as req:
-            yield req
-
-            print("Client: " + str(self.id) + " sóc el primer de la cua d'autoservei")
-
-            while(1):
-                if self.sp.caixesAuto[0].count == 1: self.utilitzaCaixaAuto(0)
-                elif self.sp.caixesAuto[1].count == 1: self.utilitzaCaixaAuto(1)'''
-
-    def utilitzaCaixaAuto(self, numCaixa):
-        '''with self.sp.caixesAuto[numCaixa].request() as req:
-            yield req
-            print("Client: " + str(self.id) + " estic comprant a la caixa d'autoservei " + numCaixa)
-            #Aqui haremos un delay dependiendo de los objetos que lleve el señor
-            pass
-        
-        print("Client: " + str(self.id) + "me las piro vampiro")'''
-        pass
-                    
-    def anarCuaTrad(self):
-        '''print("Client: " + str(self.id) + " estic a la cua tradicional")
-        with self.sp.cues[1].request() as req:
-            yield req
-
-            print("Client: " + str(self.id) + " sóc el primer de la cua tradicional")
-
-            while(1):
-                if self.sp.caixesTrad[0].count == 1: self.utilitzaCaixaTrad(0)
-                elif self.sp.caixesTrad[1].count == 1: self.utilitzaCaixaTrad(1)
-                elif self.sp.caixesTrad[2].count == 1: self.utilitzaCaixaTrad(2)
-                elif self.sp.caixesTrad[3].count == 1: self.utilitzaCaixaTrad(3)'''
-        pass
-    
-    def utilitzaCaixaTrad(self, numCaixa):
-        '''with self.sp.caixesTrad[numCaixa].request() as req:
-            yield req
-            #Aqui haremos un delay dependiendo de los objetos que lleve el señor
-            pass'''
-        pass
